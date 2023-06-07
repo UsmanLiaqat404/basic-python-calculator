d33efe11bbd7025ae9d48f115d9273653cdf9878
Usman Liaqat <117591938+UsmanLiaqat404@users.noreply.github.com>
2023-06-07 17:29:30 +0500

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,48 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Error: Cannot divide by zero"
+
+print("Welcome to the Calculator!")
+
+while True:
+    print("Please select an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "5":
+        print("Exiting the Calculator...")
+        break
+
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+
+    if choice == "1":
+        result = add(num1, num2)
+    elif choice == "2":
+        result = subtract(num1, num2)
+    elif choice == "3":
+        result = multiply(num1, num2)
+    elif choice == "4":
+        result = divide(num1, num2)
+    else:
+        print("Invalid choice!")
+        continue
+
+    print("Result:", result)
+    print()
